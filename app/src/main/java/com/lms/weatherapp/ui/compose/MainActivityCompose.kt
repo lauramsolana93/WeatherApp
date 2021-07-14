a0051d39b8fa98ddda4265ba3aac07667e1fb925
Laura Martinez <xlams@dsb.dk>
2021-07-14 14:34:36 +0200

    WIP Fix Preview

--- app/src/main/java/com/lms/weatherapp/ui/compose/MainActivityCompose.kt
+++ app/src/main/java/com/lms/weatherapp/ui/compose/MainActivityCompose.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
@@ -35,12 +36,11 @@ import com.lms.weatherapp.WeatherApplication
 import com.lms.weatherapp.common.utils.dateFormater
 import com.lms.weatherapp.common.utils.faranheidToCelsius
 import com.lms.weatherapp.common.utils.getDrawableWeather
+import com.lms.weatherapp.model.common.Metric
 import com.lms.weatherapp.ui.theme.WeatherComposeTheme
 import com.lms.weatherapp.ui.views.adapter.HourlyAdapter
 import com.lms.weatherapp.weather.factory.WeatherFactory
-import com.lms.weatherapp.weather.model.DailyForecast
-import com.lms.weatherapp.weather.model.ForecastWeather
-import com.lms.weatherapp.weather.model.HourlyWeather
+import com.lms.weatherapp.weather.model.*
 import com.lms.weatherapp.weather.repository.WeatherRepository
 import com.lms.weatherapp.weather.viewmodel.WeatherViewModel
 import com.lms.weatherapp.weather.viewmodel.WeatherViewModelFactory
@@ -128,7 +128,8 @@ class MainActivityCompose : AppCompatActivity() {
 fun ForecastWeather12hours(viewModel: WeatherViewModel?) {
     val forecast by viewModel?.forecast!!.observeAsState(initial = ForecastWeather(listOf()))
     LazyColumn(
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.Bottom
     ) {
         items(
             items = forecast.forecast,
@@ -150,19 +151,19 @@ fun WeatherListItem(dailyForecast: DailyForecast) {
         backgroundColor = Color.White,
         shape = RoundedCornerShape(corner = CornerSize(16.dp))
     ) {
-        Row {
+        Row(Modifier.fillMaxWidth()) {
             Column(
                 modifier = Modifier
                     .padding(8.dp)
                     .fillMaxSize(),
                 horizontalAlignment = Alignment.Start
             ) {
-                Text(text = dailyForecast.date.dateFormater())
+                Text(fontSize = 20.sp,text = dailyForecast.date)//dailyForecast.date.dateFormater())
                 val minTemp =
                     "${dailyForecast.temperature.minimum.value}${dailyForecast.temperature.minimum.unit}".faranheidToCelsius()
                 val maxTemp =
                     "${dailyForecast.temperature.maximum.value}${dailyForecast.temperature.maximum.unit}".faranheidToCelsius()
-                Row {
+                Row(Modifier.fillMaxWidth(), Arrangement.SpaceBetween) {
                     Text(text = "Min: $minTemp")
                     Image(
                         ImageVector.vectorResource(
@@ -170,16 +171,14 @@ fun WeatherListItem(dailyForecast: DailyForecast) {
                         ), contentDescription = "", Modifier.size(20.dp)
                     )
                 }
-                Row {
-                    Text(text = "Max: $maxTemp")
+                Row(Modifier.fillMaxWidth(), Arrangement.SpaceBetween) {
+                    Text(text = "Max: $maxTemp", fontSize = 16.sp)
                     Image(
                         ImageVector.vectorResource(id = getDrawableWeather(dailyForecast.night.icon)),
                         contentDescription = "",
                         Modifier.size(20.dp)
                     )
                 }
-
-
             }
             Column(
                 modifier = Modifier
@@ -217,4 +216,27 @@ fun LocationName(viewModel: WeatherViewModel) {
         )
     }
 
+}
+
+@Preview
+@Composable
+fun weatherForecast(){
+    val metric = Metric(100.0, "F", 1)
+    val temperature = TemperatureForecast(metric, metric)
+    val day = Day(1, "", false)
+    val night = Night(1, "", false)
+    val dailyForecast = DailyForecast(
+        "23/08/2021",
+        0L,
+        temperature = temperature,
+        day,
+        night,
+        listOf(),
+        "",
+        ""
+    )
+
+    WeatherComposeTheme() {
+        WeatherListItem(dailyForecast = dailyForecast)
+    }
 }
